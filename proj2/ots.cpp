bb7d19195b5ab7a863105e9b07860619debf61c2
Peter Kubov <peter.kubov@avast.com>
2020-03-26 18:55:37 +0100

    refactored odd-even alg

--- proj2/ots.cpp
+++ proj2/ots.cpp
@@ -27,111 +27,86 @@ void parseInput()
 	input.close();
 }
 
+void printSorted(int nproc)
+{
+	MPI_Status stat;
+
+	for (int i = 0, val; i < nproc; i++) {
+		MPI_Recv(&val, 1, MPI_INT, i, TAG, MPI_COMM_WORLD, &stat);
+		std::cout << val << std::endl;
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	int numprocs;				//pocet procesoru
-	int myid;						 //muj rank
-	int neighnumber;			//hodnota souseda
-	int mynumber;				//moje hodnota
-	MPI_Status stat;			//struct- obsahuje kod- source, tag, error
+	int nproc;       // number of processors
+	int pid;         // id of computing processor
+	int val, nval;   // value of computing (neighboring) processor
+
+	MPI_Status stat;
 
 	// Initialize MPI
-	MPI_Init(&argc, &argv);								  // inicializace MPI 
-	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);		 // zjistíme, kolik procesů běží 
-	MPI_Comm_rank(MPI_COMM_WORLD, &myid);			  // zjistíme id svého procesu 
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
+	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
-	//NACTENI SOUBORU
-	/* -proc s rankem 0 nacita vsechny hodnoty
-	 * -postupne rozesle jednotlive hodnoty vsem i sobe
-	 */
-	if(myid == 0){
+	// Processor with ID 0 does parsing of input.
+	// -> sends values to all other processors.
+	if (pid == 0)
 		parseInput();
-	}//nacteni souboru
 
-	//PRIJETI HODNOTY CISLA
-	//vsechny procesory(vcetne mastera) prijmou hodnotu a zahlasi ji
-	MPI_Recv(&mynumber, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat); //buffer,velikost,typ,rank odesilatele,tag, skupina, stat
-	//cout<<"i am:"<<myid<<" my number is:"<<mynumber<<endl;
+	// Receive value
+	// All processors including the one that sends values.
+	MPI_Recv(&val, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, &stat);
 
-	//LIMIT PRO INDEXY
-	int oddlimit= 2*(numprocs/2)-1;					  //limity pro sude
-	int evenlimit= 2*((numprocs-1)/2);				  //liche
-	int halfcycles= numprocs/2;
-	int cycles=0;											  //pocet cyklu pro pocitani slozitosti
-	//if(myid == 0) cout<<oddlimit<<":"<<evenlimit<<endl;
+	int oddN = 2*(nproc/2)-1;
+	int evenN = 2*((nproc-1)/2);
+	int N = (nproc+1)/2; // round up
 
-
-	//RAZENI------------chtelo by to umet pocitat cykly nebo neco na testy------
-	//cyklus pro linearitu
-	for(int j=1; j<=halfcycles; j++){
-		cycles++;			  //pocitame cykly, abysme mohli udelat krasnej graf:)
-
-		//sude proc 
-		if((!(myid%2) || myid==0) && (myid<oddlimit)){
-			MPI_Send(&mynumber, 1, MPI_INT, myid+1, TAG, MPI_COMM_WORLD);			 //poslu sousedovi svoje cislo
-			MPI_Recv(&mynumber, 1, MPI_INT, myid+1, TAG, MPI_COMM_WORLD, &stat);//a cekam na nizsi
-			//cout<<"ss: "<<myid<<endl;
-		}//if sude
-		else if(myid<=oddlimit){//liche prijimaji zpravu a vraceji mensi hodnotu (to je ten swap)
-			MPI_Recv(&neighnumber, 1, MPI_INT, myid-1, TAG, MPI_COMM_WORLD, &stat); //jsem sudy a prijimam
-
-			if(neighnumber > mynumber){														//pokud je leveho sous cislo vetsi
-				MPI_Send(&mynumber, 1, MPI_INT, myid-1, TAG, MPI_COMM_WORLD);		 //poslu svoje 
-				mynumber= neighnumber;															 //a vemu si jeho
+	for (int k = 0; k < N; k++) {
+		if ((pid%2 == 0) && (pid < oddN)) { // Only evens -> except last one if odd N
+			// Send message to neighbor.
+			MPI_Send(&val, 1, MPI_INT, pid+1, TAG, MPI_COMM_WORLD);
+			// Recieve message from neighbor.
+			MPI_Recv(&val, 1, MPI_INT, pid+1, TAG, MPI_COMM_WORLD, &stat);
+		}
+		else if (pid <= oddN) { // All odds (We must make sure last even does not get here if odd N)
+			// Recieve from neighbor
+			MPI_Recv(&nval, 1, MPI_INT, pid-1, TAG, MPI_COMM_WORLD, &stat);
+			if (nval > val) {
+				MPI_Send(&val, 1, MPI_INT, pid-1, TAG, MPI_COMM_WORLD);
+				val = nval;
 			}
-			else MPI_Send(&neighnumber, 1, MPI_INT, myid-1, TAG, MPI_COMM_WORLD);//pokud je mensi nebo stejne vratim
-			//cout<<"sl: "<<myid<<endl;
-		}//else if (liche)
-		else{//sem muze vlezt jen proc, co je na konci
-		}//else
-
-		//liche proc 
-		if((myid%2) && (myid<evenlimit)){
-			MPI_Send(&mynumber, 1, MPI_INT, myid+1, TAG, MPI_COMM_WORLD);			  //poslu sousedovi svoje cislo
-			MPI_Recv(&mynumber, 1, MPI_INT, myid+1, TAG, MPI_COMM_WORLD, &stat);	 //a cekam na nizsi
-			//cout<<"ll: "<<myid<<endl;
-		}//if liche
-		else if(myid<=evenlimit && myid!=0){//sude prijimaji zpravu a vraceji mensi hodnotu (to je ten swap)
-			MPI_Recv(&neighnumber, 1, MPI_INT, myid-1, TAG, MPI_COMM_WORLD, &stat); //jsem sudy a prijimam
-
-			if(neighnumber > mynumber){														//pokud je leveho sous cislo vetsi
-				MPI_Send(&mynumber, 1, MPI_INT, myid-1, TAG, MPI_COMM_WORLD);		 //poslu svoje 
-				mynumber= neighnumber;															 //a vemu si jeho
+			else {
+				MPI_Send(&nval, 1, MPI_INT, pid-1, TAG, MPI_COMM_WORLD);
 			}
-			else MPI_Send(&neighnumber, 1, MPI_INT, myid-1, TAG, MPI_COMM_WORLD);//pokud je mensi nebo stejne vratim
-			//cout<<"ls: "<<myid<<endl;
-		}//else if (sude)
-		else{//sem muze vlezt jen proc, co je na konci
-		}//else
+		}
 
-	}//for pro linearitu
-	//RAZENI--------------------------------------------------------------------
+		if ((pid%2 == 1) && (pid < evenN)) { // Only odds -> except last one if even N
+			// Send message to neighbor.
+			MPI_Send(&val, 1, MPI_INT, pid+1, TAG, MPI_COMM_WORLD);
+			// Recieve message from neighbor.
+			MPI_Recv(&val, 1, MPI_INT, pid+1, TAG, MPI_COMM_WORLD, &stat);
+		}
+		else if (pid <= evenN && pid != 0) { // All evens (We must make sure last odd does not get here if even N)
+			// Recieve from neighbor
+			MPI_Recv(&nval, 1, MPI_INT, pid-1, TAG, MPI_COMM_WORLD, &stat);
+			if (nval > val) {
+				MPI_Send(&val, 1, MPI_INT, pid-1, TAG, MPI_COMM_WORLD);
+				val = nval;
+			}
+			else {
+				MPI_Send(&nval, 1, MPI_INT, pid-1, TAG, MPI_COMM_WORLD);
+			}
+		}
+	}
 
+	MPI_Send(&val, 1, MPI_INT, 0, TAG,  MPI_COMM_WORLD);
 
-	//FINALNI DISTRIBUCE VYSLEDKU K MASTEROVI-----------------------------------
-	int* final= new int [numprocs];
-	//final=(int*) malloc(numprocs*sizeof(int));
-	for(int i=1; i<numprocs; i++){
-		if(myid == i) MPI_Send(&mynumber, 1, MPI_INT, 0, TAG,  MPI_COMM_WORLD);
-		if(myid == 0){
-			MPI_Recv(&neighnumber, 1, MPI_INT, i, TAG, MPI_COMM_WORLD, &stat); //jsem 0 a prijimam
-			final[i]=neighnumber;
-		}//if sem master
-	}//for
-
-	if(myid == 0){
-		//cout<<cycles<<endl;
-		final[0]= mynumber;
-		for(int i=0; i<numprocs; i++){
-			std::cout<<"proc: "<<i<<" num: "<<final[i]<<std::endl;
-		}//for
-	}//if vypis
-	//cout<<"i am:"<<myid<<" my number is:"<<mynumber<<endl;
-	//VYSLEDKY------------------------------------------------------------------
-
+	if(pid == 0)
+		printSorted(nproc);
 
 	MPI_Finalize(); 
 	return 0;
 
-}//main
-
+}
