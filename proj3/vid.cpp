ff7a14c2d6cc4e9df80853fed5bbf54e44cc18bc
Peter Kubov <peter.kubov@avast.com>
2020-04-18 01:51:08 +0200

    Improve computation

--- proj3/vid.cpp
+++ proj3/vid.cpp
@@ -4,14 +4,24 @@
  * Date: 16. 3. 2020
  */
 
-#include <cstring>
 #include <cmath>
+#include <cstring>
 #include <iostream>
+#include <limits>
 #include <vector>
 
 #include <mpi.h>
 
 #define TAG 0
+
+long nextpow2(int num)
+{
+	long i = 1;
+	while (num >>= 1)
+		i <<= 1;
+
+	return i == num ? i : i << 1;
+}
 
 void readInput(std::vector<double> &vals, int nproc)
 {
@@ -19,7 +29,8 @@ void readInput(std::vector<double> &vals, int nproc)
 	while (std::cin >> input)
 		vals.push_back(input);
 
-	int angsize = vals.size();
+	int angsize = nextpow2(vals.size());
+
 	for (int i = 0; i < nproc; i++)
 		MPI_Send(&angsize, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
 }
@@ -33,7 +44,7 @@ int recieveInputSize()
 	return angsize;
 }
 
-void compueAngles(int pid, int nproc, MPI_Win &win, double* angles, int n)
+void computeAngles(int pid, int nproc, MPI_Win &win, double* angles, int n)
 {
 	int gap = 2;
 	double first;
@@ -47,25 +58,31 @@ void compueAngles(int pid, int nproc, MPI_Win &win, double* angles, int n)
 	int max = ceil(((double)n)/(nproc*gap));
 	for (int i = 0; i < max; i++) {
 		int idx = 2*pid + i*nproc*2;
-		for (int j = idx; j < idx + 2; j++)
-			if (j < n)
+		for (int j = idx; j < idx + 2; j++) {
+			if (j < n) // TODO: if power of 2 this can be deleted
 				MPI_Get(&angles[j], 1, MPI_DOUBLE, 0, j, 1, MPI_DOUBLE, win);
 
-		MPI_Win_fence(0, win);
+			MPI_Win_fence(0, win);
 
-		for (int j = idx; j < idx + 2; j++)
-			if (j < n)
+			if (j < n) { // TODO: if power of 2 this can be deleted
 				angles[j] = j == 0 ? 0 : std::atan((angles[j]-first)/j);
+				MPI_Put(&angles[j], 1, MPI_DOUBLE, 0, j, 1, MPI_DOUBLE, win);
+			}
+		}
 	}
+
+	MPI_Win_fence(0, win);
 }
 
 void maxscan(int pid, int nproc, MPI_Win &win, double *angles, int n)
 {
+	// TODO: remove... just for nice output
+	MPI_Win_fence(0, win);
 	// gap -> each processor takes evey nth element of array.
 	// ngap -> gap in next level -> used to compute pid.
 	int gap = 1, ngap = 2;
 
-	for (int step = 0; step < ceil(std::log2(n)); step++) {
+	for (int step = 0; step < std::log2(n); step++) {
 		int pgap = gap; gap = ngap; ngap <<= 1;
 		int max = ceil(((double)n)/(nproc*gap));
 		for (int i = 0; i < max; i++) {
@@ -74,6 +91,8 @@ void maxscan(int pid, int nproc, MPI_Win &win, double *angles, int n)
 				double max = std::max(angles[idx], angles[idx-pgap]);
 				int tgPid = (idx/ngap)%nproc; // TODO: is modulo needed?
 				MPI_Put(&max, 1, MPI_DOUBLE, tgPid, idx, 1, MPI_DOUBLE, win);
+				if (tgPid != 0)
+					MPI_Put(&max, 1, MPI_DOUBLE, 0, idx, 1, MPI_DOUBLE, win);
 			}
 		}
 
@@ -102,6 +121,8 @@ int main(int argc, char** argv)
 	if (pid == 0)
 		readInput(angs, nproc);
 
+	// For convenience is nsize rounded to nearest power of 2
+	int origangsize = angs.size();
 	int angsize = recieveInputSize();
 
 	MPI_Win_allocate(
@@ -113,15 +134,30 @@ int main(int argc, char** argv)
 		&win
 	);
 	
-	if (pid == 0)
-		std::memcpy(angles, angs.data(), sizeof(double)*angsize);
+	if (pid == 0) {
+		angs.resize(angsize, -std::numeric_limits<double>::infinity()); // fill rest with smallest number
+		std::memcpy(angles, angs.data(), sizeof(double)*angs.size());
+	}
 
-	compueAngles(pid, nproc, win, angles, angsize);
+	computeAngles(pid, nproc, win, angles, angsize);
+
+	if (pid == 0) {
+		std::cout << "After angle compute:" << std::endl;
+		for (int i = 0; i < angsize; i++) {
+			std::cout << "\ti: " << i << " = " << angles[i] << std::endl;
+		}
+		std::cout << "====" << std::endl;
+	}
 
 	maxscan(pid, nproc, win, angles, angsize);
 
-	if (pid == 0)
+	if (pid == 0) {
+		std::cout << "After maxscan:" << std::endl;
+		for (int i = 0; i < angsize; i++) {
+			std::cout << "\ti: " << i << " = " << angles[i] << std::endl;
+		}
 		std::cout << "maxscan = " << angles[angsize-1] << std::endl;
+	}
 
 	MPI_Win_free(&win);
 	MPI_Finalize(); 
